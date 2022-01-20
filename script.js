705dd62ec7c679cb7bec195e7a95527b418c2af3
Dragos Daian <daian.dragos@yahoo.com>
2022-01-20 14:04:56 +0100

    add all images

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ window;
 gsap.ticker.fps(24)
 
 
-function createPages(pages, bookPrefix){
-  const book = document.getElementById("book")
+function createPages(pages, bookPrefix, book, startIndex = 1){
   for (let i = 0; i < pages; i++) {
     const page = document.createElement("div")
-    page.classList.add("page", "book__page")
+    page.classList.add("page", "book__page", `book__page__${bookPrefix}`)
     page.style = `--page-index: ${i + 2};`
     const page1 = document.createElement("div")
     page1.classList.add("page__half", "page__half--front")
@@ -17,32 +16,24 @@ function createPages(pages, bookPrefix){
     page2.classList.add("page__half", "page__half--back")
 
     const page1Image = document.createElement("img")
-    page1Image.src = `./images/${bookPrefix}-${i*2 + 1}.jpeg`
-    const page1Number = document.createElement("div")
-
-    page1Number.classList.add("page__number")
-    page1.appendChild(page1Number).appendChild(document.createTextNode(`${i*2 + 1}`))
+    page1Image.src = `./images/${bookPrefix}-${i*2 + startIndex}.jpeg`
 
     page1.appendChild(page1Image)
 
     const page2Image = document.createElement("img")
-    page2Image.src = `./images/${bookPrefix}-${i*2 + 2}.jpeg`
-    const page2Number = document.createElement("div")
-    const page2Header = document.createElement("div")
-    page2Header.classList.add("page__header")
-    page2Number.classList.add("page__number")
+    page2Image.src = `./images/${bookPrefix}-${i*2 + startIndex + 1}.jpeg`
     page2.appendChild(page2Image)
-    page2Header.appendChild(document.createTextNode(`SKETCHBOOK #${bookPrefix}`))
-    page2.appendChild(page2Header)
-    page2.appendChild(page2Number).appendChild(document.createTextNode(`${i*2 + 2}`))
 
     page.appendChild(page1)
     page.appendChild(page2)
     book.appendChild(page)
   }
+}
+
+function createBookEnd(pages, book) {
   const pageEnd = document.createElement("div")
   pageEnd.classList.add("page", "book__page", "book__cover", "book__cover--back")
-  pageEnd.style = `--page-index: ${pages*2 + 2};`
+  pageEnd.style = `--page-index: ${pages};`
   const backCover = document.createElement("div")
   backCover.classList.add("page__half", "page__half--back")
   const bookInset = document.createElement("div")
@@ -52,78 +43,111 @@ function createPages(pages, bookPrefix){
   book.appendChild(pageEnd)
 }
 
-function updatePageCount(pageCount){
-  const sheet = document.createElement('style')
-  sheet.innerHTML = `
-  :root {
-    --page-count: ${pageCount + 3};
+function createBook(year, pages, startIndex, onComplete, endPos) {
+
+  const book = document.getElementById(`book-${year}`)
+  createPages(pages, year, book, startIndex)
+  createBookEnd(pages, book)
+  const PAGES = [...document.querySelectorAll(`.book__page__${year}`)];
+  const pageCount = PAGES.length - 1;
+
+
+  function updatePageCount(pageCount){
+    const sheet = document.createElement('style')
+    sheet.innerHTML = `
+    :root {
+      --page-count: ${pageCount + 3};
+    }
+    `;
+    document.body.appendChild(sheet);
   }
-  `;
-  document.body.appendChild(sheet);
-}
+  
 
-createPages(19, '2017')
-const PAGES = [...document.querySelectorAll('.book__page')];
-const pageCount = PAGES.length - 1;
-updatePageCount(pageCount)
+  updatePageCount(pageCount)
 
-function pageSet(page, index){
-  if (index === pageCount) return false;
-  set(page, {
-    z: index === 0 ? pageCount : (pageCount - index) * 0.2
-  });
-}
 
-PAGES.forEach(pageSet)
+  function pageSet(page, index){
+    if (index === pageCount) return false;
+    set(page, {
+      z:index == 0 ? pageCount * 3 : (pageCount - index) * 1
+    });
+  }
 
-function pageAnimation(page, index) {
-  if (index == 0) {
-    to('.book', {
-      duration: 2,
+  PAGES.forEach(pageSet)
+  
+  const FAST_MODE = false
+
+  function initialZoom(){
+    to(`.book-${year}`, {
+      duration: 1,
+      force3D: true,
       transform: "translate(0, -50%) scale(1)"
     });
   }
-  if (index === pageCount) return false;
-  let ease = "circle"
-  let duration = 3
-  let delay = 1.8
-  to(page, {
-    rotateY: `-=${180 - index * 0.5}`,
-    z: index === 0 ? -pageCount : index * 0.2,
-    ease: ease,
-    duration: duration,
-    delay: delay * index,
-    onComplete: () => {
-      if (index == pageCount - 1) {
-        PAGES.slice().reverse().forEach(resetPages)
-      }
-    }
-  })
-}
 
-function resetPages(page, index){
-  if (index === 0) return false;
-  let ease = "circle"
-  let duration = 1.4
-  let delay = 0.3
-  to(page, {
-    rotateY: 0,
-    z: index === pageCount ? pageCount : (index) * 0.2,
-    ease: ease,
-    duration: duration,
-    delay: delay * index,
-    onComplete: () => {
-      if (index == pageCount) {
-        TweenLite.delayedCall(4, ()=> {PAGES.forEach(pageAnimation)})
-        PAGES.forEach(pageSet)
-        to('.book', {
-          duration: 2,
-          transform: "translate(-50%, -50%) scale(0.5)"
-        })
-      }
-    }
-  })
-}
+  function outroZoom(){
+    PAGES.forEach(pageSet)
+    to(`.book-${year}`, {
+      duration: 1,
+      force3D: true,
+      transform: endPos
+    })
+  }
 
-TweenLite.delayedCall(2, ()=>{PAGES.forEach(pageAnimation)});
+  function pageAnimation(page, index) {
+    if (index === pageCount) return false;
+    let ease = "circle"
+    let duration = 3
+    let delay = 1.8
+    if (FAST_MODE) {
+      duration = 1.2
+      delay = 0.2
+    }
+    to(page, {
+      rotateY: `-=${180 - index * 0.3}`,
+      z: index === 0 ? -pageCount : index * 0.2,
+      ease: ease,
+      duration: duration,
+      delay: delay * index,
+      onComplete: () => {
+        if (index == pageCount - 1) {
+          PAGES.slice().reverse().forEach(resetPages)
+        }
+      }
+    })
+  }
+
+  function resetPages(page, index){
+    if (index === 0) return false;
+    let ease = "circle"
+    let duration = 1.4
+    let delay = 0.3
+    to(page, {
+      rotateY: 0,
+      z: index === pageCount ? pageCount : (index) * 0.2,
+      ease: ease,
+      duration: duration,
+      delay: delay * index,
+      onComplete: () => {
+        if (index == pageCount - 1) {
+          outroZoom()
+          onComplete()
+        }
+      }
+    })
+  }
+  return {PAGES, startAnimation: () => {
+    initialZoom()
+    TweenLite.delayedCall(3, ()=>{PAGES.forEach(pageAnimation)})
+  }}
+}
+let firstBook = null
+const book2019 = createBook(2019, 20, 93, ()=>{firstBook.startAnimation()}, "translate(140%, -50%) scale(0.5)")
+const book2018 = createBook(2018, 27, 39, ()=>{book2019.startAnimation()}, "translate(80%, -50%) scale(0.5)")
+const book2017 = createBook(2017, 19, 1, ()=>{book2018.startAnimation()}, "translate(-190%, -50%) scale(0.5)")
+firstBook = book2017
+book2017.startAnimation();
+
+
+
 
